d48aa2dd2f360c4e86d99f5154eeb08f34761c7f
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-09-12 11:47:18 +0900

    add columns to users

---
+++ database/migrations/2019_09_12_024221_add_columns_to_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnsToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('customer_id');
+            $table->string('plan_id');
+            $table->string('subscription_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
