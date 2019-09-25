0b616e8e9e5b9b15c7f4283de45080705e5c6451
kyomachi-1 <kyomachi.so.dev1@gmail.com>
2019-09-25 19:08:34 +0900

    create rings/edit.blade.php

--- resources/views/rings/edit.blade.php
+++ resources/views/rings/edit.blade.php
@@ -6,6 +6,7 @@
             <div class="col-md-8">
                 <h2 class="mb-3">Edit Ring ： {{ $ring->ring_name }} </h2>
 
+                    <!-- 更新 -->
                     {!! Form::model($ring, ['route' => ['rings.update', $ring->id], 'method' => 'put']) !!}
 
                     <div class="form-group">
@@ -15,6 +16,15 @@
                     
                     <div class="mt-3">
                     {!! Form::submit('Update Ring', ['class' => 'btn btn-warning w-25', 'roll' => 'button']) !!}
+                    </div>
+                    
+                    {!! Form::close() !!}
+                    
+                    
+                    <!-- 削除 -->
+                    {!! Form::model($ring, ['route' => ['rings.destroy', $ring->id], 'method' => 'delete']) !!}
+                    <div class="mt-3">
+                    {!! Form::submit('Delete Ring', ['class' => 'btn btn-danger w-25', 'roll' => 'button']) !!}
                     </div>
                     
                     {!! Form::close() !!}
