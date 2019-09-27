a08ad197399c210710a3e1ec68b4bcda5480f728
kyomachi-1 <kyomachi.so.dev1@gmail.com>
2019-09-27 13:39:40 +0900

    add modal for confirmation /home.blade

--- resources/views/rings/create.blade.php
+++ resources/views/rings/create.blade.php
@@ -6,25 +6,36 @@
             <div class="col-md-8">
                 <h2 class="mb-3">Create New Ring</h2>
 
-                    {!! Form::model($ring, ['route' => 'rings.store']) !!}
-
+                    {!! Form::model($ring, ['route' => 'rings.store','onsubmit'=> 'return false']) !!}
+    
                     <div class="form-group">
                     {!! Form::label('ring_name', 'Ring Name') !!}
                     {!! Form::text('ring_name', null, ['class' => 'form-control']) !!}
                     </div>
-                    
+
                     <div class="mt-3">
-                    {!! Form::submit('Create New Ring', ['class' => 'btn btn-success w-25', 'roll' => 'button']) !!}
+                        <button type="button" class="btn btn-success" data-toggle="modal" data-target="#confirm-ring-create">
+                            Create New Ring
+                        </button>
                     </div>
-                    
-                    {!! Form::close() !!}
-                    
-                    <div class="mt-3">
-                        <a class="btn btn-secondary w-25" href="{{ route('rings.index') }}" role="button">Back to Index Ring</a>
+    
+                    <div class="modal fade" id="confirm-ring-create">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                        <h4 class="modal-title" id="modal-label">Confirm：Create New Ring ? </h4>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary w-25" data-dismiss="modal">Cancel</button>
+                                    {!! Form::button('OK', ['class' => 'btn btn-success w-25', 'onclick' => 'submit()', 'roll' => 'button']) !!}
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
+                    {!! Form::close() !!}
+                    
             </div>
         </div>
     </div>
-
 @endsection
