7d135ec813d6360c49a91d2fbaf9e069db7c2745
kyomachi-1 <kyomachi.so.dev1@gmail.com>
2019-09-27 17:33:55 +0900

    add index.blade.php /Ring creation limit

--- resources/views/rings/index.blade.php
+++ resources/views/rings/index.blade.php
@@ -27,15 +27,39 @@
                         </tbody>
                     </table>
                 @endif
-                <div class="mt-3">
-                    <a class="btn btn-primary btn-block" href="{{ route('rings.create') }}" role="button">Create New Ring</a>
-                </div>
                 
-                <!-- link_to_route でボタン作成 -->
-                <div class="mt-3">
-                    {!! link_to_route('rings.create', 'Create New Ring', null, ['class' => 'btn btn-primary btn-block', 'roll' => 'button']) !!}
+
+                <div class="mt-5">
+                    @if($customer_id === "" && $num_rings > 4)
+                        <button type="button" class="btn btn-danger btn-block" data-toggle="modal" data-target="#limit-ring-create">
+                            Rings Too Many！！
+                        </button>
+                        <p class="mt-3" align="right">
+                            {!! link_to_route('subscription', 'Click here & Please think about Paid Plan！') !!}
+                        </p>
+                        <div class="modal fade" id="limit-ring-create">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                            <h4 class="modal-title" id="modal-label">Sorry：Rings has reached the limit </h4>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary w-25" data-dismiss="modal">OK</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @else
+                        <!-- link_to_route でボタン作成 -->
+                        {!! link_to_route('rings.create', 'Create New Ring', null, ['class' => 'btn btn-primary btn-block', 'roll' => 'button']) !!}
+                    @endif
                 </div>
             </div>
         </div>
     </div>
 @endsection
+
+                <!--- button 要素でボタン作成 --->
+                    <!--<div class="mt-3">-->
+                    <!--    <a class="btn btn-primary btn-block" href="{{ route('rings.create') }}" role="button">Create New Ring</a>-->
+                    <!--</div>-->
